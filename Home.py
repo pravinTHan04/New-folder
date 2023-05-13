ad5eb3558db81b54d843647a77a03f0127a1bf02
Pravinthan <Pravinthan2000@gmail.com>
2023-05-13 19:57:11 +0530

    sdc

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import os
 import openai 
 
 # get the value of the environment variable named "HOME"
-openai.apikey = os.getenv("OPENAI_APIKEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 st.write("Hello world")
 name = st.text_input("Enter your name: ")
 if name:
