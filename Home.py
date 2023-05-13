a72ee7cd6ccf74347f06f36683404479ce54d48f
Pravinthan <Pravinthan2000@gmail.com>
2023-05-13 19:54:52 +0530

    open ai

--- Home.py
+++ Home.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import os
+import openai 
+
+# get the value of the environment variable named "HOME"
+openai.apikey = os.getenv("OPENAI_APIKEY")
 st.write("Hello world")
 name = st.text_input("Enter your name: ")
 if name:
