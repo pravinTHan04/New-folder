e7996ddbe831978c9d6d99fb44687d129aff9191
Pravinthan <Pravinthan2000@gmail.com>
2023-05-13 19:59:59 +0530

    asd

--- Home.py
+++ Home.py
@@ -6,5 +6,16 @@ import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 st.write("Hello world")
 name = st.text_input("Enter your name: ")
-if name:
+btn = st.button("sent")
+if btn:
     st.write(name) 
+    response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=name,
+                temperature=0.7,
+                max_tokens=256,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+                )
+    st.write(response)
